5777a99ca98f38f0f394eaf4b62001c49354bdcc
Sliman Elmansouri <elmansourisliman@gmail.com>
2024-02-28 18:56:38 +0100

    display a select option to choose a crypto

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import CryptoSummary from "./components/CryptoSummary";
 
 function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
+  const [selected, setSelected] = useState<Crypto | null>();
   useEffect(() => {
     const url =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
@@ -21,11 +22,28 @@ function App() {
 
   return (
     <div className="App">
-      {cryptos
-        ? cryptos.map((crypto) => {
-            return <CryptoSummary crypto={crypto} />;
-          })
-        : null}
+      <>
+        <select
+          onChange={(e) => {
+            const c = cryptos?.find((x) => x.id === e.target.value);
+            console.log(c);
+            setSelected(c);
+          }}
+          defaultValue="default"
+        >
+          <option value="default">Choose a cryptocurrency</option>
+          {cryptos
+            ? cryptos.map((crypto) => {
+                return (
+                  <option key={crypto.id} value={crypto.id}>
+                    {crypto.name}
+                  </option>
+                );
+              })
+            : null}
+        </select>
+        {selected ? <CryptoSummary crypto={selected} /> : null}
+      </>
     </div>
   );
 }
